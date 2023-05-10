37e9a03427bb0417584f375ff1c70298bac9c8dc
William Salas <willbullet52@gmail.com>
2023-05-10 15:17:32 -0500

    functions added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
+from functions import peliculas_mes, peliculas_dia, peliculas_pais, franquicia, productoras, retorno
 
 app = FastAPI()
 
@@ -35,56 +36,38 @@ async def root():
 # ese mes (nombre del mes, en str, ejemplo 'enero') historicamente
 @app.get("/movies/month/{month}")
 async def movies_month(month):
-    # here goes the logic
-    #
-    #
-    return {"mes":month, "cantidad": '' }
+    return peliculas_mes(month)
 
 
 #Se ingresa el dia y la funcion retorna la cantidad de peliculas que se estrenaron 
 # ese dia (de la semana, en str, ejemplo 'lunes') historicamente
 @app.get("/movies/day/{day}")
 async def movies_day(day):
-    # here goes the logic
-    #
-    #
-    return {"dia":day, "cantidad": '' }
+    return peliculas_dia(day)
 
 
 #Se ingresa la franquicia, retornando la cantidad de peliculas, ganancia total y promedio
-@app.get("/movies/franchise/{franchise}")
-async def movies_franchise(franchise):
-    # here goes the logic
-    #
-    #
-    return {'franquicia':franchise, 'cantidad':'', 'ganancia_total':'', 'ganancia_promedio':''}
+@app.get("/movies/franchise/{franchise_name}")
+async def movies_franchise(franchise_name:str):
+    return franquicia(franchise_name)
 
 
 #Ingresas el pais, retornando la cantidad de peliculas producidas en el mismo
 @app.get("/movies/country/{country}")
 async def movies_country(country):
-    # here goes the logic
-    #
-    #
-    return {'pais':country, 'cantidad':''}
+    return peliculas_pais(country)
 
 
 #Ingresas la productora, retornando la ganancia total y la cantidad de peliculas que produjeron
 @app.get("/movies/producer/{producer}")
 async def movies_producer(producer):
-    # here goes the logic
-    #
-    #
-    return {'productora':producer, 'ganancia_total': '', 'cantidad':''}
+    return productoras(producer)
 
 
 #'Ingresas la pelicula, retornando la inversion, la ganancia, el retorno y el año en el que se lanzo
-@app.get("/movies/info/{movie}")
-async def movies_info(movie):
-    # here goes the logic
-    #
-    #
-    return {'pelicula':movie, 'inversion':'', 'ganacia':'','retorno':'', 'año':''}
+@app.get("/movies/info/{movie_name}")
+async def movies_info(movie_name:str):
+    return retorno(movie_name)
 
 #Ingresas un nombre de pelicula y te recomienda las similares en una lista de 5 valores
 @app.get("/movies/recommend/{movie_title}")
@@ -97,6 +80,8 @@ async def movies_recommend(movie_title):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
+
+
 
 
 
