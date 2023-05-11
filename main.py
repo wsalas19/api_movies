b46022ce48d025a293ddf4612b974e2d4afdc883
William Salas <willbullet52@gmail.com>
2023-05-11 12:24:51 -0500

    endpoint function translated

--- main.py
+++ main.py
@@ -35,43 +35,43 @@ async def root():
 #Se ingresa el mes y la funcion retorna la cantidad de peliculas que se estrenaron 
 # ese mes (nombre del mes, en str, ejemplo 'enero') historicamente
 @app.get("/peliculas/mes/{mes}")
-async def movies_month(mes):
+async def peliculas_mes(mes):
     return peliculas_mes(mes)
 
 
 #Se ingresa el dia y la funcion retorna la cantidad de peliculas que se estrenaron 
 # ese dia (de la semana, en str, ejemplo 'lunes') historicamente
 @app.get("/peliculas/dia/{dia}")
-async def movies_day(dia):
+async def peliculas_dia(dia):
     return peliculas_dia(dia)
 
 
 #Se ingresa la franquicia, retornando la cantidad de peliculas, ganancia total y promedio
 @app.get("/peliculas/franquicia/{nombre_franquicia}")
-async def movies_franchise(nombre_franquicia:str):
+async def peliculas_franquicia(nombre_franquicia:str):
     return franquicia(nombre_franquicia)
 
 
 #Ingresas el pais, retornando la cantidad de peliculas producidas en el mismo
 @app.get("/peliculas/pais/{pais}")
-async def movies_country(pais):
+async def peliculas_pais(pais):
     return peliculas_pais(pais)
 
 
 #Ingresas la productora, retornando la ganancia total y la cantidad de peliculas que produjeron
 @app.get("/peliculas/productoras/{nombre_productora}")
-async def movies_producer(nombre_productora):
+async def peliculas_productora(nombre_productora):
     return productoras(nombre_productora)
 
 
 #'Ingresas la pelicula, retornando la inversion, la ganancia, el retorno y el año en el que se lanzo
 @app.get("/peliculas/retorno/{nombre_pelicula}")
-async def movies_info(nombre_pelicula:str):
+async def retorno_peliculas(nombre_pelicula:str):
     return retorno(nombre_pelicula)
 
 #Ingresas un nombre de pelicula y te recomienda las similares en una lista de 5 valores
 @app.get("/peliculas/recomendacion/{titulo_pelicula}")
-async def movies_recommend(titulo_pelicula):
+async def recomendacion_recomendacion(titulo_pelicula):
     # here goes the logic
     #
     #
